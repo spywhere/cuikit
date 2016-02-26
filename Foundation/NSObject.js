775be688a2e31c5e878bb1f914c7112a3c38dac0
Sirisak Lueangsaksri <sirisak.lu@tnis.com>
2016-02-26 14:10:20 +0700

    Add more NSObject methods

--- Foundation/NSObject.js
+++ Foundation/NSObject.js
@@ -1,6 +1,8 @@
 "use strict";
 let moduleExports = {};
 
+let objHash = require("object-hash");
+
 class NSObject {
     constructor(){
     }
@@ -18,6 +20,51 @@ class NSObject {
         return self[selector].apply(
             self, Array.prototype.slice.call(arguments, 1)
         );
+    }
+    performSelectorWithObject(selector, obj){
+        let self = this;
+
+        return self.performSelector(selector, obj);
+    }
+    performSelectorWithObjectWithObject(selector, obj1, obj2){
+        let self = this;
+
+        return self.performSelector(selector, obj1, obj2);
+    }
+    performSelectorWithObjectAfterDelay(selector, object, delay){
+        let self = this;
+
+        setTimeout(
+            () => {
+                self.performSelector(selector, object);
+            },
+            delay * 1000
+        );
+    }
+    isMemberOfClass(clazz){
+        let self = this;
+
+        return self.constructor.name === clazz.prototype.constructor.name;
+    }
+    isKindOfClass(clazz){
+        let self = this;
+
+        return self instanceof clazz;
+    }
+    isSubclassOfClass(clazz){
+        let self = this;
+
+        return !self.isMemberOfClass(clazz) && self.isKindOfClass(clazz);
+    }
+    isEqual(obj){
+        let self = this;
+
+        return self.hash() === obj.hash();
+    }
+    hash(){
+        let self = this;
+
+        return objHash(self);
     }
 }
 
